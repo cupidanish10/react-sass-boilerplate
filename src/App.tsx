92eb526d18db58abce000fc93c250625e4fbd2ea
anish-fusemachines <anish.rokka@fusemachines.com>
2023-09-13 20:43:47 +0545

    initail comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/css/main.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main className="outer-container">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="login">
+              <div className="login__header">
+                <h1> Create your Account </h1>
+                <p>Create an account to have Campaign Brain guide you to a successful Election Day</p>
+              </div>
+              <div className="login__body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">Email </label>
+                  <input type="email" className="form-control" placeholder="Enter your email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                  <div id="emailHelp" className="form-text text-danger"> Email Not Found </div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                  <input type="password" className="form-control" placeholder="create a password" id="exampleInputPassword1" />
+                </div>
+             
+                <button type="submit" className="btn btn-primary w-100 rounded-pill"> Get Started </button>
+                <div>
+                  <p> Already have an account? <Link to="/"> Login </Link></p>
+
+                </div>
+                <div className="d-flex align-items-center py-3 mb-3">
+                  <hr className="w-100" />
+                  <div className="px-5">Or</div>
+                  <hr className="w-100" />
+                </div>
+              </form>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
   );
 }
 
